529246fa6b4f10b546e85a73fcaa7808c414db50
Rafael Wielewski <rafaw99@gmail.com>
2023-01-07 10:58:31 -0300

    ip

--- src/routes.php
+++ src/routes.php
@@ -28,7 +28,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Slim\Views\PhpRenderer;
 
-header("Access-Control-Allow-Origin: http://186.211.97.189:8081");
+header("Access-Control-Allow-Origin: http://186.211.97.209:8081");
 header("Access-Control-Allow-Headers: Content-Type");
 
 
