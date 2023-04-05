038e72b11674e70e3d5d1c0375d4cd28e32abaef
Rafael Wielewski <rafaw99@gmail.com>
2023-04-05 16:00:50 -0300

    Update index.php

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,4 +1,4 @@
 <?php
-require __DIR__ . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'src'. DIRECTORY_SEPARATOR . 'index.php';
+require __DIR__  . DIRECTORY_SEPARATOR . 'src'. DIRECTORY_SEPARATOR . 'index.php';
 
 ?>
