9eba0452f4701e1d98764a3b6c7c844bf3643941
Rafael Wielewski <rafaw99@gmail.com>
2023-04-05 23:38:54 -0300

    Update http.js

--- frontend/src/services/http.js
+++ frontend/src/services/http.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   headers: {
     "Content-type": "aplication/json",
   },
-  baseURL: "https://twitter-clone-server-638gqlp3q-rafaelwielewski.vercel.app",
+  baseURL: "https://twitter-clone-server-rafaelwielewski.vercel.app",
 });
 
 export default axiosInstance;
